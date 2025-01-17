886de0d3b50993edf0006f3213838381a3e9f50c
Gustavo Zorzi <gustavozorzi2003@gmail.com>
2025-01-17 18:56:41 -0300

    up

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'zorzi23/grammi_core',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '24af6dcb6f6b0ac10d59c849da824dd70a88aa70',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'zorzi23/grammi_core' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '24af6dcb6f6b0ac10d59c849da824dd70a88aa70',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
